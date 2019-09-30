d17b3d0622cbcc6622e67f1bb08ee83ca9be0843
Jeremie Maufret <sayou.jeremie@gmail.com>
2019-09-30 22:38:35 +0200

    add Readme & parser.js

---
+++ parser.js
@@ -0,0 +1,15 @@
+const title = document.getElementsByClassName('nom_serie');
+const editor = document.getElementsByClassName('editeur');
+const nbVolume = document.getElementsByClassName('volume');
+const type = document.getElementsByClassName('color_status_complete');
+let temp = {};
+for (let i=0;i<title.length;i=i+2) {
+    const serie = {
+        id: i/2,
+        title: title[i].textContent,
+        editor: editor[i].textContent,
+        nbVolumes: nbVolume[i].textContent,
+    }
+    temp[serie.id] = serie;
+}
+JSON.stringify(temp);
